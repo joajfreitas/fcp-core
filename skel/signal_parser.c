d59f8ee86f123dde57bf3b45dc87a404a8ba3461
joajfreitas <joaj.freitas@gmail.com>
2020-07-18 19:33:31 +0100

    fix no decimal places in signal_parser

--- skel/signal_parser.c
+++ skel/signal_parser.c
@@ -73,39 +73,14 @@ function(double) \
  *  @brief Generate a bitmask with length bits set to 1.
  *  @param length Size of the bit mask
  */
-
 uint64_t bitmask(unsigned length) {
-
-	static const uint64_t bitmask_table[65] = { 
-		0x0000000000000000ULL, 0x0000000000000001ULL, 0x0000000000000003ULL,
-		0x0000000000000007ULL, 0x000000000000000fULL, 0x000000000000001fULL,
-		0x000000000000003fULL, 0x000000000000007fULL, 0x00000000000000ffULL,
-		0x00000000000001ffULL, 0x00000000000003ffULL, 0x00000000000007ffULL,
-		0x0000000000000fffULL, 0x0000000000001fffULL, 0x0000000000003fffULL,
-		0x0000000000007fffULL, 0x000000000000ffffULL, 0x000000000001ffffULL,
-		0x000000000003ffffULL, 0x000000000007ffffULL, 0x00000000000fffffULL,
-		0x00000000001fffffULL, 0x00000000003fffffULL, 0x00000000007fffffULL,
-		0x0000000000ffffffULL, 0x0000000001ffffffULL, 0x0000000003ffffffULL,
-		0x0000000007ffffffULL, 0x000000000fffffffULL, 0x000000001fffffffULL,
-		0x000000003fffffffULL, 0x000000007fffffffULL, 0x00000000ffffffffULL,
-		0x00000001ffffffffULL, 0x00000003ffffffffULL, 0x00000007ffffffffULL,
-		0x0000000fffffffffULL, 0x0000001fffffffffULL, 0x0000003fffffffffULL,
-		0x0000007fffffffffULL, 0x000000ffffffffffULL, 0x000001ffffffffffULL,
-		0x000003ffffffffffULL, 0x000007ffffffffffULL, 0x00000fffffffffffULL,
-		0x00001fffffffffffULL, 0x00003fffffffffffULL, 0x00007fffffffffffULL,
-		0x0000ffffffffffffULL, 0x0001ffffffffffffULL, 0x0003ffffffffffffULL,
-		0x0007ffffffffffffULL, 0x000fffffffffffffULL, 0x001fffffffffffffULL,
-		0x003fffffffffffffULL, 0x007fffffffffffffULL, 0x00ffffffffffffffULL,
-		0x01ffffffffffffffULL, 0x03ffffffffffffffULL, 0x07ffffffffffffffULL,
-		0x0fffffffffffffffULL, 0x1fffffffffffffffULL, 0x3fffffffffffffffULL,
-		0x7fffffffffffffffULL, 0xffffffffffffffffULL 
-	};
-
-	if (length < (sizeof(bitmask_table) / sizeof(bitmask_table[0]))) {
-        return bitmask_table[length];
+	if (length >= 64) {
+		return 0xffffffffffffffffULL;
 	}
-    else
-        return 0xffffffffffffffffULL;
+	else {
+		return (1ULL << length) - 1;
+	}
+	
 }
 
 #define apply_linear(type) \
@@ -115,7 +90,7 @@ type apply_linear_ ## type ( type bitfield, fcp_signal_t signal) { \
 		return (type) (bitfield + (type) signal.offset); \
 	} \
 	else { \
-		printf("apply_linear_" STRINGIFY(type)": %lld \n", (unsigned long long) bitfield); \
+		/*printf("apply_linear_" STRINGIFY(type)": %lld \n", (unsigned long long) bitfield); */\
 		return (type) (signal.scale * ((double) bitfield) + signal.offset);\
 	}\
 }
@@ -164,7 +139,7 @@ _type fcp_decode_signal_ ## _type (CANdata msg, fcp_signal_t signal) { \
 				big_endian_conv( (uint64_t) \
 					get_bitfield(can_word(msg), signal), signal), signal); \
 	case SIGNED: \
-		return (_type) apply_linear_int64_t( \
+		return (_type) apply_linear_ ## _type( \
 				bitfield_sign_conv( \
 					big_endian_conv( \
 						get_bitfield(can_word(msg), signal), signal), signal), signal);	 \
