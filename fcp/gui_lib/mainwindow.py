a4efd06cb4f6edd7a918f64a756d0e83ebad1f7e
joajfreitas <joaj.freitas@gmail.com>
2021-01-31 00:08:05 +0000

    Show html in nag page

--- fcp/gui_lib/mainwindow.py
+++ fcp/gui_lib/mainwindow.py
@@ -68,7 +68,7 @@ def nag_intro():
     if newest_version != VERSION:
         out += f"FCP v{newest_version} is available. Go get it:\nsudo pip install fcp=={newest_version}"
 
-    release_url = f"https://joajfreitas.gitlab.io/fcp-core/v{VERSION}.md"
+    release_url = f"https://joajfreitas.gitlab.io/fcp-core/v{VERSION}.html"
     print(release_url)
     r = requests.get(release_url)
     out += "\n" + r.text
