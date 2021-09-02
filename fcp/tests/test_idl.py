012eda90a15d60f9aa4d7733bd3e58c76b8d86f8
joajfreitas <joaj.freitas@gmail.com>
2021-09-02 14:06:05 +0100

    more tests for automatic allocation of messages

--- fcp/tests/test_idl.py
+++ fcp/tests/test_idl.py
@@ -30,3 +30,17 @@ def test_message_allocation_ordered_missing_all():
     result["c"] ["start"] = 48
 
     assert result == message_allocation(given)
+
+def test_message_allocation_unordered_missing_all():
+    given = {
+        "a": {"start": None, "length": 8,},
+        "b": {"start": None, "length": 16,},
+        "c": {"start": None, "length": 32,},
+    }
+
+    result = copy.deepcopy(given)
+    result["a"] ["start"] = 0
+    result["b"] ["start"] = 16
+    result["c"] ["start"] = 32
+
+    assert result == message_allocation(given)
