526fb26b78656d05b8ea6ffc16073cd9eab56700
João Freitas <Joao.MC.Freitas@ctw.bmwgroup.com>
2021-09-02 20:11:05 +0100

    more message allocation

--- fcp/tests/test_idl.py
+++ fcp/tests/test_idl.py
@@ -44,3 +44,27 @@ def test_message_allocation_unordered_missing_all():
     result["c"] ["start"] = 32
 
     assert result == message_allocation(given)
+
+def test_message_allocation_many_variables():
+    given = {
+        "a": {"start": None, "length": 8,},
+        "b": {"start": None, "length": 8,},
+        "c": {"start": None, "length": 8,},
+        "d": {"start": None, "length": 8,},
+        "e": {"start": None, "length": 8,},
+        "f": {"start": None, "length": 8,},
+        "g": {"start": None, "length": 8,},
+        "h": {"start": None, "length": 8,},
+    }
+
+    result = copy.deepcopy(given)
+    result["a"] ["start"] = 0
+    result["b"] ["start"] = 8
+    result["c"] ["start"] = 16
+    result["d"] ["start"] = 24
+    result["e"] ["start"] = 32
+    result["f"] ["start"] = 40
+    result["g"] ["start"] = 48
+    result["h"] ["start"] = 46
+
+    assert result == message_allocation(given)
