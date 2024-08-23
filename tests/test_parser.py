98096a5a5ddcd7ebf6e71fb3754583fe4777f6d3
João Freitas <joaj.freitas@gmail.com>
2024-08-23 23:25:22 +0100

    Basic enum test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,18 +6,18 @@ from fcp.v2_parser import get_fcp
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-@pytest.mark.parametrize("fcp,result", [("001.fcp", "001.json")])
-def test_parser(fcp, result):
+
+@pytest.mark.parametrize("test_name", ["001", "002"])
+def test_parser(test_name):
     config_dir = os.path.join(THIS_DIR, "configs")
     print("config dir", config_dir)
     fpi_config = os.path.join(config_dir, "test.fpi")
-    fcp_config = os.path.join(config_dir, fcp)
-    result = os.path.join(config_dir, result)
+    fcp_config = os.path.join(config_dir, test_name + ".fcp")
+    result = os.path.join(config_dir, test_name + ".json")
 
     fcp_v2, sources = get_fcp(fcp_config, fpi_config).unwrap()
-   
+
     fcp_json = fcp_v2.unwrap().to_dict()
     result = json.loads(open(result).read())
-    
 
     assert fcp_json == result
