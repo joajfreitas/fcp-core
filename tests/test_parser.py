61572ee4b71f383c68397ff0fb98d5fc05fb0fc9
João Freitas <joaj.freitas@gmail.com>
2024-09-13 16:47:15 +0100

    Cleanup parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -13,32 +13,14 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
     "test_name", ["001_basic_struct", "002_basic_enum", "003_comments"]
 )  # type: ignore
 def test_parser(test_name: str) -> None:
-    def drop_meta(obj: Any) -> Any:
-        if isinstance(obj, dict):
-            return {k: drop_meta(v) for k, v in obj.items() if k != "meta"}
-        elif isinstance(obj, list):
-            return [drop_meta(x) for x in obj]
-        else:
-            return obj
-
-    def drop_null_members(obj: Any) -> Any:
-        if isinstance(obj, dict):
-            return {k: drop_null_members(v) for k, v in obj.items() if v is not None}
-        elif isinstance(obj, list):
-            return [drop_null_members(x) for x in obj]
-        else:
-            return obj
-
     config_dir = os.path.join(THIS_DIR, "configs")
     fcp_config = os.path.join(config_dir, test_name + ".fcp")
     result_path = os.path.join(config_dir, test_name + ".json")
 
     fcp_v2, sources = get_fcp(fcp_config).unwrap()
 
-    # Convert FcpV2 object to dictionary
-    fcp_json_dict = json.dumps(
-        drop_null_members(drop_meta(to_dict(fcp_v2.unwrap()))), indent=2
-    )
+    # Convert FcpV2 object to JSON string and then to a dictionary
+    fcp_json_dict = fcp_v2.unwrap().to_dict()
 
     # Load the expected json result
     with open(result_path, "r") as result_file:
