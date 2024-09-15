26794b67bcb2a4a6f81ca11201c02023898e06d5
João Freitas <joaj.freitas@gmail.com>
2024-09-15 23:05:06 +0100

    Make comment an optional field

--- src/fcp/specs/comment.py
+++ src/fcp/specs/comment.py
@@ -2,6 +2,16 @@ from typing import Optional
 from serde import serde, strict
 
 
+def comment_serializer(cls, o):
+    print("ser", cls, o)
+    return None
+
+
+def comment_deserializer(cls, o):
+    print("de", cls, o)
+    return None
+
+
 @serde(type_check=strict)
 class Comment:
     value: str
