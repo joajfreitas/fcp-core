c0fe2e23e815013a6ec5c23631ab82986961d50a
João Freitas <joao.mc.freitas@ctw.bmwgroup.com>
2024-09-17 16:15:37 +0100

    Fix lints

--- src/fcp/specs/comment.py
+++ src/fcp/specs/comment.py
@@ -1,20 +1,21 @@
+from typing import Optional
 from serde import serde, strict
 
 
-def comment_serializer(comment):
+@serde(type_check=strict)
+class Comment:
+    value: str
+
+
+def comment_serializer(comment: Optional[Comment]) -> Optional[str]:
     if comment is not None:
         return comment.value
     else:
         return None
 
 
-def comment_deserializer(comment):
+def comment_deserializer(comment: Optional[str]) -> Optional[Comment]:
     if comment is not None:
-        return Comment(comment)
+        return Comment(value=comment)  # type: ignore
     else:
         return None
-
-
-@serde(type_check=strict)
-class Comment:
-    value: str
