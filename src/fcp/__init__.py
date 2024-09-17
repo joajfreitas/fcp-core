1f540b233aac96062ff92552ea5ef1ffa37dba70
João Freitas <joaj.freitas@gmail.com>
2024-09-17 20:03:05 +0100

    Fix typing issues with default serialization

--- src/fcp/__init__.py
+++ src/fcp/__init__.py
@@ -1,3 +1,4 @@
 from .specs.v2 import FcpV2 as FcpV2
+from .specs.v2 import default_serialization as default_serialization
 from .specs.device import Device as Device
 from .version import VERSION as VERSION
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ import json
 from pprint import pprint
 
 from fcp.v2_parser import get_fcp
-from fcp.v2 import default_serialization
+from fcp import default_serialization
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,32 +12,31 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 @pytest.mark.parametrize(
     "test_name",
     ["001_basic_struct", "002_basic_enum", "003_comments", "004_struct_composition"],
-) # type: ignore
+)  # type: ignore
 def test_parser(test_name: str) -> None:
     config_dir = os.path.join(THIS_DIR, "configs")
     fcp_config = os.path.join(config_dir, test_name + ".fcp")
     result_path = os.path.join(config_dir, test_name + ".json")
 
-<<<<<<< HEAD
     fcp_v2, sources = get_fcp(fcp_config).unwrap()
-=======
-    fcp_v2, _ = get_fcp(fcp_config, fpi_config).unwrap()
->>>>>>> e6e4a6b (wip)
 
     fcp_json_dict = fcp_v2.unwrap().to_dict()
 
     # Load the expected json result
     with open(result_path, "r") as result_file:
-        expected_result_dict = json.dumps(json.load(result_file), indent=2)
+        expected_result_dict = json.load(result_file)
 
+    pprint(fcp_json_dict)
+    pprint(expected_result_dict)
     assert fcp_json_dict == expected_result_dict
-    #assert 1 == 2
 
-def test_default_serialization():
+
+def test_default_serialization() -> None:
     config_dir = os.path.join(THIS_DIR, "configs")
-    fpi_config = os.path.join(config_dir, "test.fpi")
     fcp_config = os.path.join(config_dir, "004_struct_composition.fcp")
 
-    fcp_v2, _ = get_fcp(fcp_config, fpi_config).unwrap()
+    fcp_v2, _ = get_fcp(fcp_config).unwrap()
 
-    default_serialization(fcp_v2, {})
+    assert default_serialization(
+        fcp_v2.unwrap(), "baz", {"var": {"var1": 1, "var2": 2}}
+    ) == bytearray([1, 2])
