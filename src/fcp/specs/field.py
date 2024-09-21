ee86e55b1db8bc87c15a92ea7fc7dc0e8230243e
João Freitas <joaj.freitas@gmail.com>
2024-09-21 23:48:58 +0100

    Remove useless methods from Field

--- src/fcp/specs/field.py
+++ src/fcp/specs/field.py
@@ -7,11 +7,5 @@ class Field:
     name: str
     value: Any
 
-    def get_type(self) -> str:
-        return "extension_signal"
-
-    def get_name(self) -> str:
-        return self.name
-
     def __repr__(self) -> str:
         return str(to_dict(self))
