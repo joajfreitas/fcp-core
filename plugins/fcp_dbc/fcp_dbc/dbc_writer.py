d8d4e22bc7e7274cedcae9802309b0e8b56c19c1
João Freitas <joaj.freitas@gmail.com>
2024-09-24 21:33:39 +0100

    Generate dbc

--- plugins/fcp_dbc/fcp_dbc/dbc_writer.py
+++ plugins/fcp_dbc/fcp_dbc/dbc_writer.py
@@ -1,61 +1,96 @@
 from cantools.database.can.database import Database as CanDatabase
 from cantools.database.can.message import Message as CanMessage
 from cantools.database.can.signal import Signal as CanSignal
-from cantools.database.can.node import Node as CanNode
 
-from cantools.database import *
+from cantools.database import dump_file
 
 import copy
 
-
-def is_float(signal):
-    return signal.type == "float" or signal.type == "double"
+from fcp.specs import SignalBlock, Signal
 
 
 def is_signed(signal):
-    return signal.type == "signed"
-
-
-def is_decimal(signal):
-    return signal.type == "signed" or signal.type == "unsigned"
+    return signal.type[0] == "i"
 
 
 def is_multiplexer(signal, mux_signals):
     return signal.name in mux_signals
 
 
-def make_signal(signal, mux_signals, dev_name):
-    def make_signal_closure(name, signal, mux_signals, ids):
+class SignalCodec:
+    def __init__(self):
+        self.bitstart = 0
+
+    def get_fields(self):
+        return self.ext.fields if self.ext else {}
+
+    def get_bitstart(self):
+        if self.ext is not None and self.ext.fields.get("bitstart"):
+            return self.ext.fields.get("bitstart")
+        else:
+            bitstart = self.bitstart
+            self.bitstart += self.get_bitlength()
+            return bitstart
+
+    def get_bitlength(self):
+        default_types = [
+            "u8",
+            "u16",
+            "u32",
+            "u64",
+            "i8",
+            "i16",
+            "i32",
+            "i64",
+            "f32",
+            "f64",
+        ]
+        if self.signal.type in default_types:
+            return int(self.signal.type[1:])
+        else:
+            raise ValueError("Can't deal with custom types")
+
+    def get_field(self, name):
+        fields = {
+            "endianness": "little",
+            "scale": 1.0,
+            "offset": 0.0,
+            "minimum": 0,
+            "maximum": 0,
+            "device": "",
+            "endianness": "little",
+            "mux_count": None,
+        }
+
+        return self.get_fields().get(name, fields.get(name))
+
+    def convert(self, signal, extension, mux_signals):
+        self.signal = signal
+        self.ext = extension
+
         return CanSignal(
-            name,
-            signal.start + (7 if signal.byte_order == "big_endian" else 0),
-            signal.length,
-            byte_order=signal.byte_order,
-            scale=float(signal.scale),
-            offset=float(signal.offset),
-            minimum=int(signal.min_value),
-            maximum=int(signal.max_value),
-            unit=signal.unit,
-            comment=signal.comment,
-            is_float=is_float(signal),
-            is_signed=is_signed(signal),
-            decimal=is_decimal(signal),
-            is_multiplexer=is_multiplexer(signal, mux_signals),
-            multiplexer_ids=ids,
-            multiplexer_signal=[signal.mux],
-            receivers=[dev_name],
+            self.signal.name,
+            self.get_bitstart() + (7 if self.get_field("endianness") == "big" else 0),
+            self.get_bitlength(),
+            byte_order=(
+                "big_endian"
+                if self.get_field("endianness") == "big"
+                else "little_endian"
+            ),
+            is_signed=is_signed(self.signal),
+            minimum=self.get_field("minimum"),
+            maximum=self.get_field("maximum"),
+            unit=self.signal.unit or None,
+            comment=self.signal.comment or None,
+            is_multiplexer=signal.name in mux_signals,
+            multiplexer_ids=(
+                list(range(mux_count))
+                if (mux_count := self.get_field("mux_count")) is not None
+                else None
+            ),
+            multiplexer_signal=self.get_field("mux_signal"),
+            receivers=[],
         )
-
-    if signal.mux_count == 1:
-        # if len(mux_signals) > 0:
-        #    yield make_signal_closure(signal.name, signal, mux_signals, [1])
-        # else:
-        yield make_signal_closure(signal.name, signal, mux_signals, None)
-
-        return
-
-    for i in range(signal.mux_count):
-        yield make_signal_closure(signal.name + str(i), signal, mux_signals, [i])
 
 
 def process_mux_signals(signals):
@@ -67,26 +102,38 @@ def process_mux_signals(signals):
     return mux_signals
 
 
-def write_dbc(spec):
-
+def write_dbc(fcp):
     messages = []
-    nodes = []
 
-    for dev_name, dev in spec.devices.items():
-        nodes.append(CanNode(dev.name, ""))
-        for msg_name, msg in dev.msgs.items():
-            id = make_sid(dev.id, msg.id)
+    for struct in fcp.structs:
+        extension = fcp.get_matching_extension(struct, "can")
 
-            mux_signals = process_mux_signals(msg.signals)
-            signals = []
-            for sig_name, sig in msg.signals.items():
-                for sig in make_signal(sig, mux_signals, dev_name):
-                    signals.append(sig)
+        mux_signals = [
+            extension.get_signal_fields(signal.name).get("mux_signal")
+            for signal in struct.signals
+        ]
 
-            messages.append(
-                CanMessage(id, msg.name, msg.dlc, signals, senders=[dev_name])
+        mux_signals = [x for x in mux_signals if x is not None]
+
+        signal_codec = SignalCodec()
+        signals = [
+            signal_codec.convert(signal, extension.get_signal(signal.name), mux_signals)
+            for signal in struct.signals
+        ]
+
+        print(signals)
+        messages.append(
+            CanMessage(
+                frame_id=extension.fields.get("id"),
+                name=struct.name,
+                length=8,
+                signals=signals,
+                senders=[],
             )
+        )
 
-    db = CanDatabase(messages=messages, nodes=nodes)
-    return db
-    # dump_file(db, dbc)
+    db = CanDatabase(messages=messages, nodes=[])
+
+    dbc = db.as_dbc_string(sort_signals="default")
+    print(dbc)
+    return dbc
