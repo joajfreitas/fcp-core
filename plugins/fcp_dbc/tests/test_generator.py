cefc61c48e93718eb3fde2c42c64be0a4acd40d8
João Freitas <joaj.freitas@gmail.com>
2024-09-29 19:31:13 +0100

    Modify dbc generator tests

--- plugins/fcp_dbc/tests/test_generator.py
+++ plugins/fcp_dbc/tests/test_generator.py
@@ -1,136 +1,57 @@
+import os
 import pytest
-from beartype.typing import NoReturn
 
 from fcp_dbc import Generator
 
-from fcp import FcpV2
-from fcp.specs.struct import Struct
-from fcp.specs.enum import Enum, Enumeration
-from fcp.specs.signal import Signal
-from fcp.specs.metadata import MetaData
-from fcp.specs.extension import Extension
+from fcp.v2_parser import get_fcp
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-@pytest.fixture  # type: ignore
-def fcp_v2() -> FcpV2:
-    return FcpV2(
-        structs=[
-            Struct(
-                name="A",
-                signals=[
-                    Signal(
-                        name="s1",
-                        field_id=0,
-                        type="u8",
-                        meta=MetaData.default_metadata(),
-                    ),
-                    Signal(
-                        name="s2",
-                        field_id=1,
-                        type="u8",
-                        meta=MetaData.default_metadata(),
-                    ),
-                ],
-            )
-        ],
-        extensions=[
-            Extension(
-                name="A",
-                protocol="can",
-                type="A",
-                fields={"id": 10},
-                signals=[],
-                meta=MetaData.default_metadata(),
-            )
-        ],
-    )
+def get_fcp_config(name: str) -> str:
+    config_dir = os.path.join(THIS_DIR, "configs")
+    return os.path.join(config_dir, name + ".fcp")
 
 
-def test_dbc_generator(fcp_v2: FcpV2) -> NoReturn:
+def get_result_dbc(name: str) -> str:
+    config_dir = os.path.join(THIS_DIR, "configs")
+    result_path = os.path.join(config_dir, name + ".dbc")
+
+    with open(result_path, "r") as result_file:
+        return result_file.read()
+
+
+def get_bo(dbc: str) -> str:
+    sections = [section for section in dbc.split("\r\n") if section != ""]
+
+    bo_sections = [
+        section
+        for section in sections
+        if section.startswith("BO_") or section.startswith(" SG_")
+    ]
+
+    return "\n".join(bo_sections) + "\n"
+
+
+@pytest.mark.parametrize(
+    "test_name",
+    [
+        "001_basic_struct",
+        "002_nested_enum",
+        "003_nested_struct",
+    ],
+)  # type: ignore
+def test_dbc_generator(test_name: str) -> None:
+    fcp_v2, _ = get_fcp(get_fcp_config(test_name)).unwrap()
+    dbc = get_result_dbc(test_name)
     generator = Generator()
+
     results = generator.generate(
         fcp_v2,
         {"output": "output.dbc"},
     )
+    result = get_bo(results[0].get("contents"))
 
-    contents = results[0].get("contents")
-
-    lines = [line for line in contents.split("\r\n") if line != ""]
-
-    dbc = """BO_ 10 A: 8 Vector__XXX
- SG_ s2 : 8|8@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s1 : 0|8@1+ (1,0) [0|0] "" Vector__XXX"""
-
-    assert dbc == "\n".join(lines[-3:])
-
-
-def test_dbc_generator_nested_enum(fcp_v2: FcpV2) -> NoReturn:
-    generator = Generator()
-
-    fcp_v2.enums.append(
-        Enum(
-            name="E",
-            enumeration=[
-                Enumeration("S0", 0),
-                Enumeration("S1", 1),
-                Enumeration("S2", 2),
-            ],
-        )
-    )
-
-    fcp_v2.structs[0].signals.append(
-        Signal(name="s3", field_id=2, type="E", meta=MetaData.default_metadata())
-    )
-
-    results = generator.generate(fcp_v2, {"output": "output.dbc"})
-
-    contents = results[0].get("contents")
-    lines = [line for line in contents.split("\r\n") if line != ""]
-
-    dbc = """BO_ 10 A: 8 Vector__XXX
- SG_ s3 : 16|2@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s2 : 8|8@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s1 : 0|8@1+ (1,0) [0|0] "" Vector__XXX"""
-
-    assert dbc == "\n".join(lines[-4:])
-
-
-def test_dbc_generator_nested_struct(fcp_v2: FcpV2) -> NoReturn:
-    generator = Generator()
-
-    fcp_v2.structs.append(
-        Struct(
-            name="B",
-            signals=[
-                Signal(
-                    name="s1",
-                    field_id=0,
-                    type="u8",
-                    meta=MetaData.default_metadata(),
-                ),
-                Signal(
-                    name="s2",
-                    field_id=1,
-                    type="u8",
-                    meta=MetaData.default_metadata(),
-                ),
-            ],
-        )
-    )
-
-    fcp_v2.structs[0].signals.append(
-        Signal(name="s3", field_id=2, type="B", meta=MetaData.default_metadata())
-    )
-
-    results = generator.generate(fcp_v2, {"output": "output.dbc"})
-
-    contents = results[0].get("contents")
-    lines = [line for line in contents.split("\r\n") if line != ""]
-
-    dbc = """BO_ 10 A: 8 Vector__XXX
- SG_ s3_s2 : 24|8@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s3_s1 : 16|8@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s2 : 8|8@1+ (1,0) [0|0] "" Vector__XXX
- SG_ s1 : 0|8@1+ (1,0) [0|0] "" Vector__XXX"""
-
-    assert dbc == "\n".join(lines[-5:])
+    print(result)
+    print(dbc)
+    assert result == dbc
