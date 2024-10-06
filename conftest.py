e85f82a2c42a620ff15fb86be2cc685677701b25
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-10-06 16:03:45 +0100

    Started c_gen

--- conftest.py
+++ conftest.py
@@ -2,3 +2,4 @@ import sys
 
 sys.path.append("plugins/fcp_dbc")
 sys.path.append("plugins/fcp_nop")
+sys.path.append("plugins/fcp_can")
---
+++ plugins/fcp_can_c/fcp_can_c/generator.py
@@ -0,0 +1,41 @@
+from beartype.typing import Any, Union, Dict, NoReturn
+from pathlib import Path
+
+from fcp.codegen import CodeGenerator
+from fcp.verifier import register, Verifier
+from fcp.result import Result, Err, Ok
+from fcp import FcpV2
+from fcp.error import FcpError
+from fcp.types import Nil
+
+from .can_c_writer import write_can_c
+
+
+class Generator(CodeGenerator):
+    def __init__(self) -> None:
+        pass
+
+    def generate(self, fcp: FcpV2, ctx: Any) -> Dict[str, Union[str, Path]]:
+        return [
+            {
+                "type": "file",
+                "path": Path(ctx.get("output")),
+                "contents": write_can_c(fcp).unwrap(),
+            }
+        ]
+
+    def register_checks(self, verifier: Verifier) -> NoReturn:
+        @register(verifier, "extension")  # type: ignore
+        def check_extension_valid_type(
+            self: Any, fcp: FcpV2, extension: Any
+        ) -> Result[Nil, FcpError]:
+            struct = fcp.get_struct(extension.type)
+            if struct.is_nothing():
+                return Err(
+                    FcpError(
+                        f"No matching type for extension {extension.name}",
+                        node=extension,
+                    )
+                )
+            else:
+                return Ok(())
