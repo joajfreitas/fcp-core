d904021501b006fbd4e69dc333e876b77cf673fa
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-10-06 16:03:45 +0100

    Started c_gen

---
+++ plugins/fcp_can_c/README.md
@@ -0,0 +1,61 @@
+# FCP CAN C generator
+
+fcp plugin to generate CAN bus C librtaries
+
+## Usage
+
+```
+$ fcp generate can_c <fcp source> <output file>
+```
+
+## Supported fields
+
+### Impl
+
+ * **id**: CAN bus frame identifier (sid)
+ * **device**: Sender device name
+
+### SignalBlock
+
+ * **mux_count**: Number of variants for muxed signal
+ * **mux_signal**: Signal that muxes the current signal
+ * **endianess**: byte order of the encoded signal
+ * **bitstart**: Start bit for the current signal
+ * **scale**: Scaling multiplier for the encoded signal
+ * **offset**: Scaling offset for the encoded signal
+ * **minimum**: Allowed minimum value for the decoded signal
+ * **maximum**: Allowed maximum value for the decoded signal
+
+
+## Example
+
+```fcp
+version: "3"
+
+enum SensorState {
+    Off = 0,
+    On = 1,
+    Error = 2,
+}
+
+struct SensorInformation {
+    temperature @0: f32 | unit("C"),
+    sensors_state @1: SensorState,
+    sensor_id @2: u8,
+}
+
+impl can for SensorInformation {
+    id: 100,
+    device: "ecu1",
+
+    signal temperature {
+        mux_count: 16,
+        mux_signal: "sensor_id",
+    },
+
+    signal sensor_state {
+        mux_count: 16,
+        mux_signal: "sensor_id",
+    },
+}
+```
---
+++ plugins/fcp_can_c/fcp_can_c/can_c_writer.py
@@ -0,0 +1,83 @@
+from beartype.typing import List
+from math import ceil
+
+from cantools.database.can.database import Database as CanDatabase
+from cantools.database.can.message import Message as CanMessage
+from cantools.database.can.signal import Signal as CanSignal
+
+from fcp.specs import Signal
+from fcp import FcpV2
+
+from fcp.maybe import Some
+from fcp.result import Result
+from fcp.maybe import catch
+from fcp.encoding import make_encoder, EncodeablePiece, Value
+
+
+def is_signed(value: Value) -> bool:
+    return bool(value.type[0] == "i")
+
+
+def make_signals(encoding: List[EncodeablePiece]) -> List[Signal]:
+    signals = []
+
+    mux_signals = [
+        piece.extended_data.get("mux_signal")
+        for piece in encoding
+        if piece.extended_data.get("mux_signal") is not None
+    ]
+
+    for piece in encoding:
+        if piece.extended_data.get("mux_count") is not None:
+            mux_ids = list(range(0, piece.extended_data.get("mux_count")))
+        else:
+            mux_ids = None
+
+        signals.append(
+            CanSignal(
+                piece.name.replace("::", "_"),
+                (piece.bitstart + 7) if piece.endianess != "little" else piece.bitstart,
+                piece.bitlength,
+                byte_order=(
+                    "big_endian" if piece.endianess == "big" else "little_endian"
+                ),
+                is_signed=is_signed(piece),
+                minimum=0,
+                maximum=0,
+                unit=None,
+                comment=None,
+                is_multiplexer=piece.name in mux_signals,
+                multiplexer_ids=mux_ids,
+                multiplexer_signal=piece.extended_data.get("mux_signal"),
+            )
+        )
+
+    dlc = ceil((piece.bitstart + piece.bitlength) / 8)
+
+    return signals, dlc
+
+
+@catch  # type: ignore
+def write_dbc(fcp: FcpV2) -> Result[str, str]:
+    messages = []
+
+    encoder = make_encoder("packed", fcp)
+
+    for extension in fcp.get_matching_extensions("can"):
+        encoding = encoder.generate(extension)
+
+        signals, dlc = make_signals(encoding)
+
+        messages.append(
+            CanMessage(
+                frame_id=extension.fields.get("id"),
+                name=extension.name,
+                length=dlc,
+                signals=signals,
+                senders=[],
+            )
+        )
+
+    db = CanDatabase(messages=messages, nodes=[])
+
+    return Some(str(db.as_dbc_string(sort_signals="default")))
