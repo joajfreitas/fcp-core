e67ee8023548dd8da72bdf760fd21753c7576a6a
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-10-13 12:53:34 +0100

    signal_parser

--- plugins/fcp_can_c/fcp_can_c/can_c_writer.py
+++ plugins/fcp_can_c/fcp_can_c/can_c_writer.py
@@ -1,17 +1,36 @@
-from beartype.typing import List
+from pathlib import Path
+from beartype.typing import Tuple, List, Dict, Any, Union, NoReturn, Optional
 from math import ceil
-from typing import Tuple
+
 from cantools.database.can.database import Database as CanDatabase
 from cantools.database.can.message import Message as CanMessage
-from cantools.database.can.signal import Signal as CanSignal
+from cantools.database.can.node import Node as CanNode
 
 from fcp.specs import Signal
 from fcp import FcpV2
 
-from fcp.maybe import Some
+from dataclasses import dataclass
 from fcp.result import Result, Ok, Err
 from fcp.maybe import catch
 from fcp.encoding import make_encoder, EncodeablePiece, Value
+
+
+@dataclass
+class CanSignal:
+    name: str
+    start: int
+    type: str
+    length: int
+    is_signed: bool
+    type: str
+    byte_order: str
+    minimum: Optional[str]
+    maximum: Optional[str]
+    unit: Optional[str]
+    comment: Optional[str]
+    is_multiplexer: bool
+    multiplexer_ids: List[int]
+    multiplexer_signal: Optional[str]
 
 
 def is_signed(value: Value) -> bool:
@@ -20,6 +39,7 @@ def is_signed(value: Value) -> bool:
 
 def make_signals(encoding: List[EncodeablePiece]) -> Tuple[List[Signal], int]:
     signals = []
+    dlc = 0
 
     mux_signals = [
         piece.extended_data.get("mux_signal")
@@ -28,22 +48,25 @@ def make_signals(encoding: List[EncodeablePiece]) -> Tuple[List[Signal], int]:
     ]
 
     for piece in encoding:
-        if piece.extended_data.get("mux_count") is not None:
-            mux_ids = list(range(0, piece.extended_data.get("mux_count")))
-        else:
-            mux_ids = None
+        breakpoint()
+
+        mux_count = piece.extended_data.get("mux_count")
+        mux_ids = list(range(0, mux_count)) if mux_count is not None else []
 
         signals.append(
             CanSignal(
-                piece.name.replace("::", "_"),
-                (piece.bitstart + 7) if piece.endianess != "little" else piece.bitstart,
-                piece.bitlength,
+                name=piece.name.replace("::", "_"),
+                start=(piece.bitstart + 7)
+                if piece.endianess != "little"
+                else piece.bitstart,
+                type=piece.type,
+                length=piece.bitlength,
                 byte_order=(
                     "big_endian" if piece.endianess == "big" else "little_endian"
                 ),
                 is_signed=is_signed(piece),
-                minimum=0,
-                maximum=0,
+                minimum=None,
+                maximum=None,
                 unit=None,
                 comment=None,
                 is_multiplexer=piece.name in mux_signals,
@@ -52,15 +75,14 @@ def make_signals(encoding: List[EncodeablePiece]) -> Tuple[List[Signal], int]:
             )
         )
 
-    dlc = ceil((piece.bitstart + piece.bitlength) / 8)
+        dlc = ceil((piece.bitstart + piece.bitlength) / 8)
 
     return signals, dlc
 
 
-@catch  # type: ignore
-def write_dbc(fcp: FcpV2) -> Result[str, str]:
+def init_can_data(fcp: FcpV2) -> Tuple[List[CanMessage], List[CanNode]]:
     messages = []
-
+    devices = []
     encoder = make_encoder("packed", fcp)
 
     for extension in fcp.get_matching_extensions("can"):
@@ -68,16 +90,46 @@ def write_dbc(fcp: FcpV2) -> Result[str, str]:
 
         signals, dlc = make_signals(encoding)
 
+        id = extension.fields.get("id")
+        if id is None:
+            return Err("No id field found in extension").unwrap()
+
+        device = extension.fields.get("device")
+        device_name = device if device is not None else "Global"
+
+        if device_name not in devices:
+            devices.append(CanNode(device_name))
+
         messages.append(
             CanMessage(
-                frame_id=extension.fields.get("id"),
+                frame_id=id,
                 name=extension.name,
                 length=dlc,
                 signals=signals,
-                senders=[extension.fields.get("device")],
+                senders=[device_name],
             )
         )
 
-    db = CanDatabase(messages=messages, nodes=[])
+    return messages, devices
 
-    return Ok(str(db.as_dbc_string(sort_signals="default")))
+
+class CanCWritter:
+    def __init__(self, fcp: FcpV2, output_dir: Path) -> None:
+        self.messages, self.devices = init_can_data(fcp)
+        self.output_dir = output_dir
+
+    def get_device_dict(self, device: str) -> Dict[str, Any]:
+        return {
+            "device": device,
+            "msgs": [msg for msg in self.messages if device in msg.senders],
+        }
+
+    def get_device_header(self, device: str) -> str:
+        msgs = self.get_device_msgs(device)
+
+        raise NotImplementedError
+
+    def get_device_source(self, device: str) -> str:
+        msgs = self.get_device_msgs(device)
+
+        raise NotImplementedError
