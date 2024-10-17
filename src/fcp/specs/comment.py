130d40bec5d0e27aba2e37c2e5733b6edff02660
João Freitas <joaj.freitas@gmail.com>
2024-10-17 20:05:31 +0100

    Add docstrings to comment.py

--- src/fcp/specs/comment.py
+++ src/fcp/specs/comment.py
@@ -3,10 +3,13 @@ from serde import serde, strict
 
 @serde(type_check=strict)
 class Comment:
+    """Comment AST node."""
+
     value: str
 
 
 def comment_serializer(comment: Comment) -> str:
+    """Serialize comment with custom rules in pyserde."""
     if comment is not None:
         return comment.value
     else:
@@ -14,6 +17,7 @@ def comment_serializer(comment: Comment) -> str:
 
 
 def comment_deserializer(comment: str) -> Comment:
+    """Deserialize comment with custom rules in pyserde."""
     if comment is not None:
         return Comment(value=comment)  # type: ignore
     else:
