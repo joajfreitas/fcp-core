90243d6418212b52e3994ceed237cd1fc9830711
João Freitas <joaj.freitas@gmail.com>
2024-10-21 18:58:17 +0100

    Add docstrings to colors.py methods

--- src/fcp/colors.py
+++ src/fcp/colors.py
@@ -2,42 +2,54 @@ from termcolor import colored
 
 
 class Color:
+    """Color string with term codes."""
+
     @staticmethod
     def red(s: str) -> str:
+        """Color string red."""
         return str(colored(s, "red"))
 
     @staticmethod
     def yellow(s: str) -> str:
+        """Color string yellow."""
         return str(colored(s, "yellow"))
 
     @staticmethod
     def orange(s: str) -> str:
+        """Color string orange."""
         return str(colored(s, "light_red"))
 
     @staticmethod
     def blue(s: str) -> str:
+        """Color string blue."""
         return str(colored(s, "blue"))
 
     @staticmethod
     def white(s: str) -> str:
+        """Color string white."""
         return str(colored(s, "white"))
 
     @staticmethod
     def boldred(s: str) -> str:
+        """Color string bold red."""
         return str(colored(s, "red", attrs=["bold"]))
 
     @staticmethod
     def boldyellow(s: str) -> str:
+        """Color string bold yellow."""
         return str(colored(s, "yellow", attrs=["bold"]))
 
     @staticmethod
     def boldorange(s: str) -> str:
+        """Color string bold orange."""
         return str(colored(s, "light_red", attrs=["bold"]))
 
     @staticmethod
     def boldblue(s: str) -> str:
+        """Color string bold blue."""
         return str(colored(s, "blue", attrs=["bold"]))
 
     @staticmethod
     def boldwhite(s: str) -> str:
+        """Color string bold white."""
         return str(colored(s, "white", attrs=["bold"]))
