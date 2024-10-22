38dcee7aeee2a59caf5de923de52a5af5e051521
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-10-22 13:14:54 +0100

    const modifiers on CanFrame's

--- plugins/fcp_can_c/templates/can_signal_parser.h
+++ plugins/fcp_can_c/templates/can_signal_parser.h
@@ -9,7 +9,7 @@
 extern "C" {
 #endif
 
-uint64_t can_decode_signal_as_uint64_t(CanFrame *msg, uint64_t start,
+uint64_t can_decode_signal_as_uint64_t(const CanFrame *msg, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
@@ -17,7 +17,7 @@ uint64_t can_encode_signal_from_uint64_t(uint64_t signal, uint64_t start,
                                          uint64_t length, double scale,
                                          double offset);
 
-uint32_t can_decode_signal_as_uint32_t(CanFrame *msg, uint64_t start,
+uint32_t can_decode_signal_as_uint32_t(const CanFrame *msg, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
@@ -25,7 +25,7 @@ uint64_t can_encode_signal_from_uint32_t(uint32_t signal, uint64_t start,
                                          uint64_t length, double scale,
                                          double offset);
 
-uint16_t can_decode_signal_as_uint16_t(CanFrame *msg, uint64_t start,
+uint16_t can_decode_signal_as_uint16_t(const CanFrame *msg, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
@@ -33,7 +33,7 @@ uint64_t can_encode_signal_from_uint16_t(uint16_t signal, uint64_t start,
                                          uint64_t length, double scale,
                                          double offset);
 
-uint8_t can_decode_signal_as_uint8_t(CanFrame *msg, uint64_t start,
+uint8_t can_decode_signal_as_uint8_t(const CanFrame *msg, uint64_t start,
                                      uint64_t length, double scale,
                                      double offset);
 
@@ -41,7 +41,7 @@ uint64_t can_encode_signal_from_uint8_t(uint8_t signal, uint64_t start,
                                         uint64_t length, double scale,
                                         double offset);
 
-double can_decode_signal_as_double(CanFrame *msg, uint64_t start,
+double can_decode_signal_as_double(const CanFrame *msg, uint64_t start,
                                    uint64_t length, double scale,
                                    double offset);
 
@@ -49,14 +49,14 @@ uint64_t can_encode_signal_from_double(double signal, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
-float can_decode_signal_as_float(CanFrame *msg, uint64_t start, uint64_t length,
-                                 double scale, double offset);
+float can_decode_signal_as_float(const CanFrame *msg, uint64_t start,
+                                 uint64_t length, double scale, double offset);
 
 uint64_t can_encode_signal_from_float(float signal, uint64_t start,
                                       uint64_t length, double scale,
                                       double offset);
 
-int64_t can_decode_signal_as_int64_t(CanFrame *msg, uint64_t start,
+int64_t can_decode_signal_as_int64_t(const CanFrame *msg, uint64_t start,
                                      uint64_t length, double scale,
                                      double offset);
 
@@ -64,7 +64,7 @@ uint64_t can_encode_signal_from_int64_t(int64_t signal, uint64_t start,
                                         uint64_t length, double scale,
                                         double offset);
 
-int32_t can_decode_signal_as_int32_t(CanFrame *msg, uint64_t start,
+int32_t can_decode_signal_as_int32_t(const CanFrame *msg, uint64_t start,
                                      uint64_t length, double scale,
                                      double offset);
 
@@ -72,7 +72,7 @@ uint64_t can_encode_signal_from_int32_t(int32_t signal, uint64_t start,
                                         uint64_t length, double scale,
                                         double offset);
 
-int16_t can_decode_signal_as_int16_t(CanFrame *msg, uint64_t start,
+int16_t can_decode_signal_as_int16_t(const CanFrame *msg, uint64_t start,
                                      uint64_t length, double scale,
                                      double offset);
 
@@ -80,7 +80,7 @@ uint64_t can_encode_signal_from_int16_t(int16_t signal, uint64_t start,
                                         uint64_t length, double scale,
                                         double offset);
 
-int8_t can_decode_signal_as_int8_t(CanFrame *msg, uint64_t start,
+int8_t can_decode_signal_as_int8_t(const CanFrame *msg, uint64_t start,
                                    uint64_t length, double scale,
                                    double offset);
 
@@ -88,7 +88,7 @@ uint64_t can_encode_signal_from_int8_t(int8_t signal, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
-double can_decode_signal_as_double(CanFrame *msg, uint64_t start,
+double can_decode_signal_as_double(const CanFrame *msg, uint64_t start,
                                    uint64_t length, double scale,
                                    double offset);
 
@@ -96,14 +96,14 @@ uint64_t can_encode_signal_from_double(double signal, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
-float can_decode_signal_as_float(CanFrame *msg, uint64_t start, uint64_t length,
-                                 double scale, double offset);
+float can_decode_signal_as_float(const CanFrame *msg, uint64_t start,
+                                 uint64_t length, double scale, double offset);
 
 uint64_t can_encode_signal_from_float(float signal, uint64_t start,
                                       uint64_t length, double scale,
                                       double offset);
 
-float can_decode_signal_float_as_float(CanFrame *msg, uint64_t start,
+float can_decode_signal_float_as_float(const CanFrame *msg, uint64_t start,
                                        uint64_t length, double scale,
                                        double offset);
 
@@ -111,7 +111,7 @@ uint64_t can_encode_signal_float_from_float(float signal, uint64_t start,
                                             uint64_t length, double scale,
                                             double offset);
 
-double can_decode_signal_double_as_double(CanFrame *msg, uint64_t start,
+double can_decode_signal_double_as_double(const CanFrame *msg, uint64_t start,
                                           uint64_t length, double scale,
                                           double offset);
 
