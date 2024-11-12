09bb0b2cc1100345a670e261fd94fdde302dbbad
João Freitas <joaj.freitas@gmail.com>
2024-11-12 22:51:21 +0000

    Split signed test from basic struct

--- plugins/fcp_cpp/tests/001_basic_struct.cpp
+++ plugins/fcp_cpp/tests/001_basic_struct.cpp
@@ -21,33 +21,6 @@ UTEST(BasicStruct, EncodeSimpleUnsignedStruct) {
     EXPECT_TRUE(foo==expected);
 }
 
-UTEST(BasicStruct, DecodeSignedAndUnsignedStruct) {
-    auto foo = fcp::S2{1,2,3,4,5,6,7,8,9,10};
-    std::vector<std::uint8_t> encoded = foo.encode().GetData();
-
-    std::vector<uint8_t> bytes{
-        1,
-        2,
-        3,0,
-        4,0,
-        5,0,0,
-        6,0,0,
-        7,0,0,0,
-        8,0,0,0,
-        9,0,0,0,0,0,0,0,
-        10,0,0,0,0,0,0,0};
-
-    EXPECT_TRUE(encoded==bytes);
-}
-
-UTEST(BasicStruct, EncodeSignedAndUnsignedStruct) {
-    std::vector<uint8_t> bytes{1,2,3,4,5,6,7,8,9,10};
-
-    auto foo = fcp::S2::Encode(bytes.begin(), bytes.end());
-    auto expected = fcp::S2{1,2,3,4,5,6,7,8,9,10};
-    EXPECT_TRUE(foo==expected);
-}
-
 UTEST(BasicStruct, GetDeviceName) {
     EXPECT_TRUE(fcp::can::get_device_name(10) == "ecu1");
 }
