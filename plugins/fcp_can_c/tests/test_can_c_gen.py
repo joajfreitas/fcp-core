fc9befbe1467d9369c38ed5b2839f0d205fa9ce2
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-11-13 18:16:52 +0000

    fixes

--- plugins/fcp_can_c/tests/test_can_c_gen.py
+++ plugins/fcp_can_c/tests/test_can_c_gen.py
@@ -6,9 +6,21 @@ from fcp_can_c import Generator
 from fcp.v2_parser import get_fcp
 
 
+base_dir = os.getcwd().split("/")[-1]
+
+
 def get_fcp_config(test_dir: str, name: str) -> str:
     config_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), test_dir)
     return os.path.join(config_dir, "test.fcp")
+
+
+def get_path(test_name: str) -> str:
+    if base_dir == "fcp_can_c":
+        return f"tests/{test_name}"
+    elif base_dir == "fcp-core":
+        return f"plugins/fcp_can_c/tests/{test_name}"
+    else:
+        raise ValueError(f"Invalid testsinvocation directory: {base_dir}")
 
 
 @pytest.mark.parametrize(
@@ -20,22 +32,21 @@ def get_fcp_config(test_dir: str, name: str) -> str:
     ],
 )
 def test_can_c_gen(test_name: str):
-    fcp_v2, _ = get_fcp(Path(get_fcp_config(test_name, test_name))).unwrap()
+    fcp_v2, _ = get_fcp(Path(get_path(test_name) + "/test.fcp")).unwrap()
     generator = Generator()
 
     # Use test_name in the output path for generated files
-    output_path = f"{test_name}/generated_code"
+    output_path = get_path(test_name) + "/generated_code"
     results = generator.gen(fcp_v2, None, None, os.path.join("tests", output_path))
 
     # List contents of the directory before running make
-    print(f"Contents of {output_path}:")
     for root, dirs, files in os.walk(output_path):
         for name in files:
             print(os.path.join(root, name))
 
     print("------->", os.getcwd())
     result = subprocess.run(
-        ["make", "-C", os.path.join("tests", test_name), "run_tests"],
+        ["make", "-C", os.path.join(get_path(test_name)), "run_tests"],
         capture_output=True,
         text=True,
     )
