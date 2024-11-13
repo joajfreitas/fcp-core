4ff89e9c008ed75bed35a6d47583c09d0d222a6b
Diogo Ferreira <itsnotsoftware@gmail.com>
2024-11-13 18:16:52 +0000

    fixes

--- conftest.py
+++ conftest.py
@@ -2,5 +2,5 @@ import sys
 
 sys.path.append("plugins/fcp_dbc")
 sys.path.append("plugins/fcp_nop")
-sys.path.append("plugins/fcp_can")
+sys.path.append("plugins/fcp_can_c")
 sys.path.append("plugins/fcp_cpp")
