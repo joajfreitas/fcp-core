717d200d96020feca980150b671bc9a50ef0742d
João Freitas <joaj.freitas@gmail.com>
2024-11-19 19:20:00 +0000

    Update set_version.sh

--- .github/workflows/set_version.sh
+++ .github/workflows/set_version.sh
@@ -3,4 +3,4 @@
 version_string=$(git rev-parse HEAD | head -c 8)
 version_string=$(echo "ibase=16; $(echo "$version_string" | tr 'a-z' 'A-Z')" | bc)
 
-sed -i "s|version = \"\([0-9]*\).\([0-9]*\).\([0-9]*\)\"|version = \"\1\.\2\.\3.dev$version_string\"|g" $1
+sed -i "s|version = \"\([0-9]*\).\([0-9]*\).\([0-9]*\).dev\([0-9]*\)\"|version = \"\1\.\2\.\3.dev$version_string\"|g" $1
