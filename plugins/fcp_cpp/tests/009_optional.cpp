c8a62756233850db0eeef4a7a56c14b0c82b7bb5
João Freitas <joaj.freitas@gmail.com>
2024-11-23 15:06:32 +0000

    fixup! Add test for optional decoding/encoding in cpp

--- plugins/fcp_cpp/tests/009_optional.cpp
+++ plugins/fcp_cpp/tests/009_optional.cpp
@@ -6,16 +6,16 @@
 UTEST_MAIN()
 
 UTEST(BasicStruct, EncodeOptionalWithValue) {
-    auto s1 = fcp::S1::Some(1);
+    auto s1 = fcp::S1{{1}};
     std::vector<std::uint8_t> encoded = s1.encode().GetData();
 
-    std::vector<uint8_t> bytes{0x01, 0x0};
+    std::vector<uint8_t> bytes{0x01, 0x1};
 
     EXPECT_TRUE(encoded==bytes);
 }
 
 UTEST(BasicStruct, EncodeOptionalWithNoValue) {
-    auto s1 = fcp::S1::None();
+    auto s1 = fcp::S1{fcp::Optional<fcp::Unsigned<std::uint8_t, 8>>::None()};
     std::vector<std::uint8_t> encoded = s1.encode().GetData();
 
     std::vector<uint8_t> bytes{0x00};
@@ -27,7 +27,7 @@ UTEST(BasicStruct, DecodeOptionalWithValue) {
     std::vector<uint8_t> bytes{0x01, 0x01};
 
     auto foo = fcp::S1::Decode(bytes.begin(), bytes.end());
-    auto expected = fcp::S1::Some(1);
+    auto expected = fcp::S1{{1}};
 
     EXPECT_TRUE(foo==expected);
 }
@@ -36,7 +36,7 @@ UTEST(BasicStruct, DecodeOptionalWithNoValue) {
     std::vector<uint8_t> bytes{0x00};
 
     auto foo = fcp::S1::Decode(bytes.begin(), bytes.end());
-    auto expected = fcp::S1::None();
+    auto expected = fcp::S1{fcp::Optional<fcp::Unsigned<std::uint8_t, 8>>::None()};
 
     EXPECT_TRUE(foo==expected);
 }
