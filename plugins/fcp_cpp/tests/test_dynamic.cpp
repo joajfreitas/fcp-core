ef0895b2ea5f3d304a93bd00dfe495dd727455a8
João Freitas <joaj.freitas@gmail.com>
2025-02-22 21:08:23 +0000

    More dynamic decoding tests

--- plugins/fcp_cpp/tests/test_dynamic.cpp
+++ plugins/fcp_cpp/tests/test_dynamic.cpp
@@ -36,7 +36,26 @@ TEST_F(DynamicSchemaTest, DecodeSimpleUnsignedStruct)
     auto decoded = schema.DecodeJson("S1", { 1, 2 });
 
     EXPECT_THAT(decoded, Optional(Eq(json { { "s1", 1 }, { "s2", 2 } })));
+}
 
+
+TEST_F(DynamicSchemaTest, DecodeIntegerStruct)
+{
+    auto schema = GetSchema();
+
+    auto decoded = schema.DecodeJson("S5", { 1, 2, 3, 0, 4, 0, 5, 0, 0, 6, 0, 0, 7, 0, 0, 0, 8, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0 });
+
+    EXPECT_THAT(decoded, Optional(Eq(json { { "s1", 1 }, { "s2", 2 }, { "s3", 3 }, { "s4", 4 }, { "s5", 5 }, { "s6", 6 }, { "s7", 7 }, { "s8", 8 }, { "s9", 9 }, { "s10", 10 } })));
+}
+
+
+TEST_F(DynamicSchemaTest, DecodeStaticArray)
+{
+    auto schema = GetSchema();
+
+    auto decoded = schema.DecodeJson("S3", { 1, 2, 3, 4, 5, 6 });
+
+    EXPECT_THAT(decoded, Optional(Eq(json { { "s1", { 1, 2, 3, 4 } }, { "s2", 5 }, { "s3", 6 } })));
 
 
 }
