246577eee2ecf6d6c9db75c46ffbfaebf8b7d3a5
fcbranco <kiko.castelobranco@gmail.com>
2025-03-22 12:09:02 +0000

    Add test files for 006_rpc_support (currently empty)

---
+++ plugins/fcp_can_c/tests/006_rpc_support/test.c
@@ -0,0 +1,35 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "generated_code/can_frame.h"
+#include "generated_code/ecu_can.h"
+
+bool all_tests_passed = true;
+
+#define VERIFY_TEST(condition)                             \
+    if (condition) {                                       \
+        printf("\033[32m [PASSED] %s\n", __func__);        \
+    } else {                                               \
+        printf("\033[31m [FAILED] %s\033[0m\n", __func__); \
+        all_tests_passed = false;                          \
+    }
+
+#define ASSERT_TESTS()      \
+    printf("\033[0m");      \
+    if (all_tests_passed) { \
+        exit(EXIT_SUCCESS); \
+    } else {                \
+        exit(EXIT_FAILURE); \
+    }
+
+
+//void random_test()
+
+int main() {
+    // Calling tests
+
+    ASSERT_TESTS();
+
+    return 0;
+}
