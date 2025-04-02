57dda3a49d22d3b371e76ee0e1466d72d8e15575
João Freitas <joaj.freitas@gmail.com>
2025-04-03 00:07:22 +0100

    Add test for missing inner optional type

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -90,6 +90,7 @@ def test_parser(test_name: str) -> NoReturn:
         "005_missing_type",
         "006_missing_dynamic_array_inner_type",
         "007_missing_array_inner_type",
+        "008_missing_optional_inner_type",
     ],
 )  # type: ignore
 def test_parsing_errors(test_name: str) -> NoReturn:
