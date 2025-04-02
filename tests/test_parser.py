b7cf15cd0ed0f06e3e101b0c4dc4eeef61d89a29
João Freitas <joaj.freitas@gmail.com>
2025-04-02 23:55:01 +0100

    Add test for missing struct tag

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -86,6 +86,7 @@ def test_parser(test_name: str) -> NoReturn:
         "001_missing_version",
         "002_wrong_version",
         "003_missing_struct_identifier",
+        "004_missing_struct_field_tag",
     ],
 )  # type: ignore
 def test_parsing_errors(test_name: str) -> NoReturn:
