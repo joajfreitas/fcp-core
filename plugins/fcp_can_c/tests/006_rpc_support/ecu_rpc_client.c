27e021cf715b622e819ab942479c24e21d6226d3
fcbranco <kiko.castelobranco@gmail.com>
2025-05-07 01:25:01 +0100

    Updating testing (in development)

--- plugins/fcp_can_c/tests/006_rpc_support/ecu_rpc_client.c
+++ plugins/fcp_can_c/tests/006_rpc_support/ecu_rpc_client.c
@@ -12,20 +12,18 @@ static void capture_response(const CanFrame *frame) {
     memcpy(&rpc_response, frame, sizeof(CanFrame));
 }
 
-bool request_sensor_state(uint8_t *result) { //sensorservice_requeststate
-    CanFrame request;
-    request.id = ECU_RPC_GET_ID;
-    request.dlc = 8;
+bool sensorservice_requeststate(uint8_t *result) {
+    CanFrame request = {
+        .id = ECU_RPC_GET_ID,
+        .dlc = 3
+    };
 
-    RpcMessage *msg = (RpcMessage *) request.data;
-    msg->rpc_get_id = ECU_RPC_GET_ID;
-    msg->rpc_ans_id = ECU_RPC_ANS_ID;
-    memset(&msg->payload, 0, sizeof(RpcPayload));
+    RpcMethod *method = (RpcMethod *) &request.data[0];
+    method->service = SENSOR_SERVICE_ID;
+    method->method = 1;
 
-    /* Add the can_service_dispatch function declaration from can_service.h */
     can_service_dispatch(&request, capture_response);
 
-    RpcMessage *res = (RpcMessage *) rpc_response.data;
-    *result = res->payload.sensorservice_requeststate_res.result;
+    *result = rpc_response.data[2];
     return true;
 }
--- plugins/fcp_can_c/tests/006_rpc_support/ecu_rpc_client.h
+++ plugins/fcp_can_c/tests/006_rpc_support/ecu_rpc_client.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+
 #include "generated_code/ecu_rpc.h"
 #include "generated_code/can_frame.h"
 
@@ -10,12 +11,11 @@
 
 /* SensorService RPC Methods */
 bool request_sensor_state(uint8_t *result);
-bool get_sensor_temperature(uint8_t *result);
 
 /* Dispatch function to handle CAN RPC requests */
 void can_service_dispatch(const CanFrame *request, void (*response_callback)(const CanFrame *));
 
 /* For test compatibility */
-#define call_my_rpc request_sensor_state //caps
+#define CALL_MY_RPC REQUEST_SENSOR_STATE
 
 #endif // ECU_RPC_CLIENT_H
