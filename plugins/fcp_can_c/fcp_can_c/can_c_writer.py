c2073b003170cba8adf7281e9d820d0b569162c6
fcbranco <kiko.castelobranco@gmail.com>
2025-07-25 19:21:39 +0100

    Changed .fcp for testing with multiple services

--- plugins/fcp_can_c/fcp_can_c/can_c_writer.py
+++ plugins/fcp_can_c/fcp_can_c/can_c_writer.py
@@ -271,6 +271,7 @@ def initialize_can_data(
             method.name_snake = to_snake_case(method.name)
             method.input_snake = to_snake_case(method.input)
             method.output_snake = to_snake_case(method.output)
+            # method.unique_id = (service.id << 8) | method.id
 
     for extension in fcp.impls:
         encoding = encoder.generate(extension)
