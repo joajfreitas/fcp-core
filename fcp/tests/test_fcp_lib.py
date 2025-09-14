8bd2105479eec8b96bd1e602b3e668d49f9fbfe3
João Freitas <joaj.freitas@gmail.com>
2025-09-14 22:42:23 +0100

    Improve test coverage

--- fcp/tests/test_fcp_lib.py
+++ fcp/tests/test_fcp_lib.py
@@ -1,7 +1,11 @@
+from attr import s
 import pytest
+
+from fcp.validator import validate
 
 from ..fcp_lib import *
 from ..can import CANMessage
+
 
 @pytest.fixture
 def fcp():
@@ -10,27 +14,59 @@ def fcp():
 
     spec = Spec()
     spec.decompile(j)
+    if len(validate(spec)) != 0:
+        return None
     return Fcp(spec)
+
 
 @pytest.fixture
 def test_msg(fcp):
-    return fcp.encode_msg("iib_motor", {"temp_motor":10})
+    return fcp.encode_msg("iib_motor", {"temp_motor": 10})
 
-def test_encode_msg(fcp, test_msg):
-    msg = fcp.encode_msg("iib_motor", {"temp_motor":10})
+
+def test_encode_msg(fcp):
+    msg = fcp.encode_msg("iib_motor", {"temp_motor": 10})
     assert type(msg) is CANMessage
     assert msg.get_dev_id() == 16
+
 
 def test_decode_msg(fcp, test_msg):
     name, signals = fcp.decode_msg(test_msg)
     assert name == "iib_motor"
     assert signals["temp_motor0"] == 10
 
+
+@pytest.fixture
+def test_muxed_msg(fcp):
+    return fcp.encode_msg("iib_diff", {"iib_diff_steer_ang": 0})
+
+
+def test_decode_muxed_msg(fcp, test_muxed_msg):
+    """Test if non multiplexed signal is mistakenly interpreted as multiplexed signal in message with other multiplexed signals"""
+    name, signals = fcp.decode_msg(test_muxed_msg)
+    assert name == "iib_diff"
+    assert "iib_diff_steer_ang" in signals.keys()
+
+
+@pytest.fixture
+def test_mux_signal(fcp):
+    return fcp.encode_msg("iib_diff", {"n_iib_diff": 2})
+
+
+def test_decode_mux_signal(fcp, test_mux_signal):
+    """Test if multiplexer signal is correct and not defaulted from another value with mux_count = 1"""
+    name, signals = fcp.decode_msg(test_mux_signal)
+    assert name == "iib_diff"
+    assert "iib_diff_value2" in signals.keys()
+    assert "iib_diff_steer_ang" in signals.keys()
+
+
 def test_encode_cmd(fcp):
-    msg = fcp.encode_cmd(10, "iib", "set_regen_on", [1,2,3])
+    msg = fcp.encode_cmd(10, "iib", "set_regen_on", [1, 2, 3])
     assert msg is not None
 
-class FakeProxy():
+
+class FakeProxy:
     def __init__(self, socket, addrs):
         self.socket = socket
         self.addrs = []
