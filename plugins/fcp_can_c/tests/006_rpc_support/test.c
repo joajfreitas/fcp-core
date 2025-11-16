afe89fd31f7ef619fb57fe255e38d0ec96df19a1
simao345 <simao.m.silveira@gmail.com>
2025-11-16 03:05:11 +0000

    adding rpc tests back

--- plugins/fcp_can_c/tests/006_rpc_support/test.c
+++ plugins/fcp_can_c/tests/006_rpc_support/test.c
@@ -24,47 +24,52 @@ bool all_tests_passed = true;
 
 /*------------------- RPC Encode/Decode Tests -------------------*/
 void test_rpc_sensor_information() {
-    CanRpcSensorInformation msg = {
-        .result = 0xA5
-    };
-
-    // Encode
+    CanRpcSensorInformation msg = {.result = 0xA5};
     CanFrame frame = can_encode_rpc_sensor_information(&msg);
     printf("Encoded frame ID: 0x%X, DLC: %d\n", frame.id, frame.dlc);
-
     VERIFY_TEST(frame.id == ECU_RPC_GET_ID || frame.id == ECU_RPC_ANS_ID);
 
-    // Decode
     CanRpcSensorInformation decoded = can_decode_rpc_sensor_information(&frame);
     VERIFY_TEST(decoded.result == msg.result);
 }
 
 void test_rpc_sensor_request() {
-    CanRpcSensorReq msg = {
-        .request_id = 0x01
-    };
-
-    // Encode
+    CanRpcSensorReq msg = {.request_id = 0x01};
     CanFrame frame = can_encode_rpc_sensor_req(&msg);
     VERIFY_TEST(frame.id == ECU_RPC_GET_ID || frame.id == ECU_RPC_ANS_ID);
 
-    // Decode
     CanRpcSensorReq decoded = can_decode_rpc_sensor_req(&frame);
     VERIFY_TEST(decoded.request_id == msg.request_id);
 }
 
 void test_rpc_temperature_response() {
-    CanRpcTemperatureResponse msg = {
-        .result = 25
-    };
-
-    // Encode
+    CanRpcTemperatureResponse msg = {.result = 25};
     CanFrame frame = can_encode_rpc_temperature_response(&msg);
     VERIFY_TEST(frame.id == ECU_RPC_GET_ID || frame.id == ECU_RPC_ANS_ID);
 
-    // Decode
     CanRpcTemperatureResponse decoded = can_decode_rpc_temperature_response(&frame);
     VERIFY_TEST(decoded.result == msg.result);
+}
+
+/*------------------- Table-driven Tests -------------------*/
+void test_rpc_sensor_information_table() {
+    uint8_t test_values[] = {0x00, 0xFF, 0xA5, 0x5A};
+    for (size_t i = 0; i < sizeof(test_values)/sizeof(test_values[0]); i++) {
+        CanRpcSensorInformation msg = {.result = test_values[i]};
+        CanFrame frame = can_encode_rpc_sensor_information(&msg);
+        CanRpcSensorInformation decoded = can_decode_rpc_sensor_information(&frame);
+        VERIFY_TEST(decoded.result == test_values[i]);
+    }
+}
+
+void test_rpc_temperature_response_table() {
+    int8_t test_values[] = {0, 25, 85, 127};
+    for (size_t i = 0; i < sizeof(test_values)/sizeof(test_values[0]); i++) {
+        CanRpcTemperatureResponse msg = {.result = test_values[i]};
+        CanFrame frame = can_encode_rpc_temperature_response(&msg);
+        CanRpcTemperatureResponse decoded = can_decode_rpc_temperature_response(&frame);
+        VERIFY_TEST(decoded.result == test_values[i]);
+    }
 }
 
 /*------------------- Main -------------------*/
@@ -72,6 +77,9 @@ int main() {
     test_rpc_sensor_information();
     test_rpc_sensor_request();
     test_rpc_temperature_response();
+
+    test_rpc_sensor_information_table();
+    test_rpc_temperature_response_table();
 
     ASSERT_TESTS();
     return 0;
